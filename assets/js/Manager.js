cdf3a1bc55301c05d2fcc47e14801425cc43249d
mfrisch87 <mikey.frisch@gmail.com>
2021-03-29 10:00:07 -0700

    close to working

--- assets/js/Manager.js
+++ assets/js/Manager.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require("./employee")
+
+class Manager extends Employee {
+    constructor(name, id, email, office_number) {
+        super(name, id, email)
+        this.office_number = office_number;
+    }
+
+    get_role() {
+        return "Manager"
+    }
+
+    get_office_number() {
+        return this.office_number
+    }
+
+}
+
+module.exports = Manager;
